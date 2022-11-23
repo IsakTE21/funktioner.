2d4b3ee2394dffacc5bcb778cf020996672b2283
IsakTE21 <111966431+IsakTE21@users.noreply.github.com>
2022-11-23 20:47:18 +0100

    Create funktioner.py

---
+++ funktioner.py
@@ -0,0 +1,54 @@
+# Övning 3 går ut på att implementera så många av nedanstående funktioner som möjligt
+# Ta bort pass och implementera funktionerna
+
+
+def best(name):
+    # TODO Skriv ut att namnet är bäst
+    # Ex: "Katherine" in - skriver ut "Katherine är bäst"
+    print(name, "är bäst.")
+
+name = (input("Vad heter du?))
+    (name)
+
+
+def square(number):
+    # TODO Returnera talet kvadrerat
+    # Ex: 5 in - 25 ut
+    print(number**2)
+
+number = int(input("Skriv in ett tal för att få kvadraten"))
+square(number)
+
+
+    
+
+
+
+def sums(a, b):
+    # TODO Returnera summan av a och b
+    # Ex: 2, 6 in - 8 ut
+    return(a+b)
+    a = int(input("Skriv in ett nummer.))
+    b = int(input("Skriv in ett till nummer"))
+    c = sums(a, b)
+
+
+    
+
+
+# Nu blir det lite svårare
+
+# TODO Returnera det största av a,b,c
+    # Ex: 3, 6, 12 in - 12 ut
+def maximum(a, b, c):
+    if: 
+        a>b and a>c: return a
+    elif: 
+        b>c return b
+    else: 
+        return c
+a = int(input("Välj ett nummer."))
+b = int(input("Välj ett nummer."))
+c = int(input("Välj ett nummer."))
+values_list = [a, b, c]
+max(values_list)
